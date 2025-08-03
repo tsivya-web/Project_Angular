42ea91ed00e751a876ef31ec7952577459c1ccf8
tsivya-web <tsivyacohen@gmail.com>
2025-08-03 17:15:49 +0300

    aa

--- frontend/Project_Angular/src/app/app.config.ts
+++ frontend/Project_Angular/src/app/app.config.ts
@@ -8,7 +8,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withDisabledInitialNavigation()),
+    provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient()
   ]
