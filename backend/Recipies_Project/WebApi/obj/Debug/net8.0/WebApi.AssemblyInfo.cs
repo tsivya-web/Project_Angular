87d319a030b79a327fa4fea2d69e51fe1d54c5be
tsivya-web <tsivyacohen@gmail.com>
2025-08-08 03:39:14 +0300

    add images

--- backend/Recipies_Project/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ backend/Recipies_Project/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131b3e6a8315dac80842a7d94ab7686db22e44b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c11ded21aedd38a0aa45eb9112091c9e50034c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
